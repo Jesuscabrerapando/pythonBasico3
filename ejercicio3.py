ea0fbdf210e8d6a6dd69c9fbaf2b7e4a7781dc42
Jesuscabrerapando <118173613+Jesuscabrerapando@users.noreply.github.com>
2022-11-16 13:37:46 +0100

    Delete ejercicio3.py

--- ejercicio3.py
+++
@@ -1,7 +0,0 @@
-def notaexamen():
-    correctas=int(input('dime cuantas preguntas has acertado'))
-    incorrectas=int(input('dime cuantas preguntas has fallado'))
-    sinresponder=int(input('dime cuantas preguntas hay sin responder'))
-    notaexamen=(correctas*0.5)-(incorrectas*0.25)+(sinresponder*0)
-    print(notaexamen)
-notaexamen()
